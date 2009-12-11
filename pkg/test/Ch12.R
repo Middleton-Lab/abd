cdca694477ce516a55ce28363d37894c17e2ba1f
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-11 18:52:17 +0000

    Fixes to build successfully
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/test/Ch12.R
@@ -0,0 +1,9 @@
+# Chapter 12
+
+setwd('~/Dropbox/Classes/BIOL 490 - 2010-01 Biometry/Whitlock/abd-12/pkg')
+
+##########################################################################
+# 12q19
+ElectricFish <- read.csv('./Book_datasets_original/12/12q19ElectricFish.csv', stringsAsFactors = FALSE)
+save(ElectricFish, file = './data/ElectricFish.rda')
+
---
+++ pkg/test/Ch21.R
@@ -0,0 +1,7 @@
+# Chapter 21
+
+setwd('~/Dropbox/Classes/BIOL 490 - 2010-01 Biometry/Whitlock/abd-12/pkg')
+
+##########################################################################
+# selection
+load('data/selection.rda')
