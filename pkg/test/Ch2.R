9dff6eafe95a45e3a291b2a1d746ee89944d1abb
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-11 05:14:28 +0000

    Major .Rd updates. Trying to get a version that will build.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test/Ch2.R
+++ pkg/test/Ch2.R
@@ -4,32 +4,42 @@ setwd('~/Dropbox/Classes/BIOL 490 - 2010-01 Biometry/Whitlock/abd-12/pkg')
 
 ##########################################################################
 # 2.1-1
+# TeenDeaths$Cause <- as.character(TeenDeaths$Cause)
 load('pkg/data/TeenDeaths.rda')
 
 str(TeenDeaths)
 TeenDeaths
 
 op <- par(no.readonly = TRUE)
-par(mai = c(2, 0.82, 0.25, 0.42), xaxs = 'i', yaxs = 'i')
+par(mai = c(2, 0.82, 0.25, 0.42),
+  xaxs = 'i',
+  yaxs = 'i')
 
-barplot(TeenDeaths$No.deaths, names.arg = TeenDeaths$Cause, las = 3, 
-	cex.axis = 0.75, cex.names = 0.75, ylim = c(0, 7000),
-	ylab = 'Number of Cases (frequency)',
+barplot(TeenDeaths$No.deaths,
+  names.arg = TeenDeaths$Cause,
+  las = 3,
+  cex.axis = 0.75,
+  cex.names = 0.75,
+  ylim = c(0, 7000),
+  ylab = 'Number of Cases (frequency)',
 	col = 'red')
 
 par(op)
 
 ##########################################################################
 # 2.1-2
+# ***** as.character
 load('pkg/data/DesertBirds.rda')
 
 str(DesertBirds)
 DesertBirds
 
-hist(DesertBirds$Count, breaks = 12, 
-	ylab = 'Frequency (Number of Species)',
-	xlab = 'Abundance',
-	main = '', col = 'red')
+hist(DesertBirds$Count,
+  breaks = 12,
+  ylab = 'Frequency (Number of Species)',
+  xlab = 'Abundance',
+  main = '',
+  col = 'red')
 
 ##########################################################################
 # 2.1-4
@@ -41,14 +51,16 @@ summary(SockeyeFemaleBodyMass)
 op <- par(no.readonly = TRUE)
 
 dev.new(width = 9, height = 3)
-par(mfrow = c(1, 3), xaxs = 'i', yaxs = 'i')
+par(mfrow = c(1, 3),
+  xaxs = 'i',
+  yaxs = 'i')
 for (breaks in c(30, 10, 5)){
-	hist(SockeyeFemaleBodyMass$mass, breaks = breaks,
-	xlim = c(1, 4),
-	col = 'red',
-	ylab = 'Frequency',
-	xlab = 'Body mass (kg)',
-	main = '')
-	}
+  hist(SockeyeFemaleBodyMass$mass, breaks = breaks,
+  xlim = c(1, 4),
+  col = 'red',
+  ylab = 'Frequency',
+  xlab = 'Body mass (kg)',
+  main = '')
+}
 
 par(op)
