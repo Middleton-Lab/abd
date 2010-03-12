a597a850494397c7b56fce4f1cfa84c6e1b8f496
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-12 19:51:08 +0000

    Ch 6 Update
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test/Ch6.R
+++ pkg/test/Ch6.R
@@ -3,20 +3,18 @@
 library(abd)
 
 ##########################################################################
-# 04e1	HumanGeneLengths.csv
-Examples:
-Example 12.2: Blackbird testosterone ----Hasselquist, D., J. A. Marsh, P. W. Sherman, and J. C. Wingfield. 1999. Behavioral Ecology and Sociobiology 45: 167–175.
-Example 12.3: Horned lizards ----Young, A. J., et al. 2006. Proceedings of the National Academy of Sciences (USA) 103: 12005–12010.
-Example 12.4: Brook trout/chinook ----Levin, P. S., S. Achord, B. E. Fiest, and R. W. Zabel. 2002. Proceedings of the Royal Society of London, Series B, Biological Sciences 269: 1663–1670.
-Problems:
-Question 12.2: No Smoking Day ----reference.
-Question 12.3:Iguanas ----reference.
-Question 12.5:Monogamy and Testes ----reference.
-Question 12.9: Cichlids ----reference.
-Question 12.10: Tobacco ----reference.
-Question 12.11: "Will"s and presidents ----reference.
-Question 12.13: Ostrich temperatures ----reference.
-Question 12.15: Primate WPCs ----reference.
-Question 12.16: Stalkie eyespan ----reference.
-Question 12.19: Electric fish ----reference.
-Question 12.23: Weddell seal dives ----reference.
+# Table 6.2-1
+
+Toads <- data.frame(n.toads = c(0:18),
+                    prob = c(0.000004, 0.00007, 0.0006, 0.0031, 0.0117,
+                             0.0327, 0.0708, 0.1214, 0.1669, 0.1855,
+                             0.1669, 0.1214, 0.0708, 0.0327, 0.0117,
+                             0.0031, 0.0006, 0.00007, 0.000004))
+save(toads, file = "Toads.rda")
+data(Toads)
+Toads
+
+barplot(Toads$prob,
+  ylim = c(0, 0.20),
+  names.arg = Toads$n.toads,
+  cex.names = 0.75)
