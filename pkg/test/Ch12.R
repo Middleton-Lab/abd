f2b4e4ce3627b65989d7b57bc23cb0f862a0a7d9
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-15 23:20:14 +0000

    HornedLizards
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test/Ch12.R
+++ pkg/test/Ch12.R
@@ -33,3 +33,74 @@ ci(BlackbirdTestosterone$dif.in.logs)
 
 qt(0.05/2, df = 12, lower.tail = FALSE)
 
+
+##########################################################################
+# 12e3	HornedLizards.csv
+#data(HornedLizards)
+#HornedLizards <- HornedLizards[-105, ]
+#row.names(HornedLizards) <- 1:184
+#HornedLizards$Squamosal.horn.length <- as.numeric(as.character(HornedLizards$Squamosal.horn.length))
+#save(HornedLizards, file = "HornedLizards.rda")
+#prompt(HornedLizards)
+
+data(HornedLizards)
+str(HornedLizards)
+
+# Subset living and killed. Drop the Survive column as well.
+living <- subset(HornedLizards, Survive == 1)$Squamosal.horn.length
+killed <- subset(HornedLizards, Survive == 0)$Squamosal.horn.length
+
+# Plot histograms of living and killed
+dev.new()
+par(mfrow = c(2, 1))
+hist(living, main = "Living", xlab = "Horn Length (mm)")
+hist(killed, main = "Killed", xlab = "Horn Length (mm)")
+
+# Confidence interval for the difference of two means
+df.l <- 153
+df.k <- 29
+df.tot <- df.l + df.k
+(y.bar.l <- mean(living))
+(y.bar.k <- mean(killed))
+(y.bar.diff <- y.bar.l - y.bar.k)
+
+(var.pooled <- (df.l * var(living) + df.k * var(killed)) / df.tot)
+
+# Note that se below uses n (not n - 1)
+(se.diff.means <- sqrt(var.pooled * (1/154 + 1/30)))
+
+# A two-sided test, so we need 0.05/2 on each side
+(t.crit <- qt(1-(0.05/2), df = df.tot))
+
+# Lower 95%
+y.bar.diff - t.crit * se.diff.means
+
+# Upper 95%
+y.bar.diff + t.crit * se.diff.means
+
+# Calculate the t-statistic for a two-sample t-test
+(t.stat <- y.bar.diff / se.diff.means)
+pt(t.stat, df = df.tot, lower.tail = FALSE)
+
+# 1. t-test assuming equal variances with t.test()
+t.test(living, killed, var.equal = TRUE)
+
+# 2. Convert Survive to a factor and use t.test() with a formula
+#    Not necessary to convert to factor, but useful for pretty output
+HornedLizards$Survive <- factor(HornedLizards$Survive, levels = c(1, 0), labels = c("Living", "Killed"))
+str(HornedLizards)
+t.test(Squamosal.horn.length ~ Survive, data = HornedLizards, var.equal = TRUE)
+
+# 3. Welch's t-test not assuming equal variances, the t.test() default
+t.test(living, killed, var.equal = FALSE)
+
+
+##########################################################################
+# 12e4	BrookTrout.csv
+data(BrookTrout)
+names(BrookTrout)[1] <- "brook.trout"
+BrookTrout$brook.trout <- as.character(BrookTrout$brook.trout)
+BrookTrout$brook.trout <- ifelse(BrookTrout$brook.trout == "+", "present", "absent")
+BrookTrout$brook.trout <- factor(BrookTrout$brook.trout)
+str(BrookTrout)
+
