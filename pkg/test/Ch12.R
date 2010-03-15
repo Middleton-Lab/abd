e7276926ad3a6f02ab4e494b6f4e4f57153c4cef
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-15 04:17:00 +0000

    BlackbirdTestosterone
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test/Ch12.R
+++ pkg/test/Ch12.R
@@ -1,9 +1,35 @@
 # Chapter 12
 
-setwd("~/Dropbox/Classes/BIOL 490 - 2010-01 Biometry/Whitlock/abd/pkg")
+library(abd)
+
+setwd("/Users/kmm/Dropbox/Classes/BIOL_490_-_2010-01_Biometry/Whitlock/abd/pkg/test")
 
 ##########################################################################
-# 12q19
-ElectricFish <- read.csv("./Book_datasets_original/12/12q19ElectricFish.csv", stringsAsFactors = FALSE)
-save(ElectricFish, file = "./data/ElectricFish.rda")
+# 12e2	BlackbirdTestosterone.csv
+data(BlackbirdTestosterone)
+BlackbirdTestosterone
+
+plot(log.after ~ log.before, data = BlackbirdTestosterone,
+  xlim = c(3.9, 5.1), ylim = c(3.9, 5.1),
+  pch = 16, col = "red",
+  ylab = "Antibody production after implant",
+  xlab = "Antibody production before implant")
+abline(b = 1, a = 0)
+
+hist(BlackbirdTestosterone$dif.in.logs,
+  xlab = "Difference (before - after)", main = "",
+  col = "red")
+
+(d.bar <- mean(BlackbirdTestosterone$dif.in.logs))
+(s.d <- sd(BlackbirdTestosterone$dif.in.logs))
+(n <- length(BlackbirdTestosterone$dif.in.logs))
+(se.d <- se(BlackbirdTestosterone$dif.in.logs))
+
+require(gmodels)
+ci(BlackbirdTestosterone$dif.in.logs)
+
+(t.stat <- (d.bar - 0)/se.d)
+2 * pt(t.stat, df = 12, lower.tail = TRUE)
+
+qt(0.05/2, df = 12, lower.tail = FALSE)
 
