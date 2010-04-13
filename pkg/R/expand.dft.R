7aaacd50142001769d50a976899878b57fd0d60f
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-13 19:55:21 +0000

    expand.dft() more general, now takes a col.exp argument
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/expand.dft.R
+++ pkg/R/expand.dft.R
@@ -1,13 +1,26 @@
 # see https://stat.ethz.ch/pipermail/r-help/2009-January/185561.html
 # for discussion of expand.dft(). Modified for abd package.
 
-expand.dft <- function(x, na.strings = "NA", as.is = FALSE, dec = "."){
-  DF <- sapply(1:nrow(x), function(i) x[rep(i, each = x$Frequency[i]), ], 
+expand.dft <- function(x, col.exp, na.strings = "NA", as.is = FALSE, dec = "."){
+  to.expand <- eval(substitute(x[a], list(a = col.exp)))[, 1]
+  DF <- sapply(1:nrow(x), function(i) x[rep(i, each = to.expand[i]), ], 
     simplify = FALSE)
-  DF <- subset(do.call("rbind", DF), select = -Frequency)
+  DF <- subset(do.call("rbind", DF), select = -(get(col.exp)))
   for (i in 1:ncol(DF)){
     DF[[i]] <- type.convert(as.character(DF[[i]]),
       na.strings = na.strings, as.is = as.is, dec = dec)
     }
 DF
 } 
+
+#expand.dft <- function(x, col.exp, na.strings = "NA", as.is = FALSE, dec = "."){
+#  
+#  DF <- sapply(1:nrow(x), function(i) x[rep(i, each = x$Frequency[i]), ], 
+#    simplify = FALSE)
+#  DF <- subset(do.call("rbind", DF), select = -Frequency)
+#  for (i in 1:ncol(DF)){
+#    DF[[i]] <- type.convert(as.character(DF[[i]]),
+#      na.strings = na.strings, as.is = as.is, dec = dec)
+#    }
+#DF
+#} 
