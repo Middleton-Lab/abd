b586cebe11f18f606d1c397c78b25c2ea742ddf8
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-26 04:56:46 +0000

    Ch 8 updates
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/prop.ci.R
+++ pkg/R/prop.ci.R
@@ -1,6 +1,11 @@
 # Agresti-Coull method for confidence intervals for proportions
 
 prop.ci <- function(X, n, conf.int = 0.95){
+  if (X %% 1 != 0) stop("X must be an integer.")
+  if (n %% 1 != 0) stop("n must be an integer.")
+  if (conf.int <= 0 | conf.int >= 1) stop("conf.int must be between 0 and 1.")
+  if (X > n) stop("X must be less than or equal to n.")
+  
   p.prime <- (X + 2) / (n + 4)
   Z <- abs(qnorm((1 - conf.int)/2))
   x <- sqrt( (p.prime * (1 - p.prime)) / (n + 4) )
@@ -16,8 +21,8 @@ prop.ci <- function(X, n, conf.int = 0.95){
 print.prop.ci <- function(x, digits = 3, ...){
   cat("\n")
   cat("Confidence interval for a proportion\n")
-  cat("\tcalculated using the Agresti-Coull (1998)\n")
-  cat("\t method for 'small samples.'\n")
+  cat("calculated using the Agresti-Coull (1998)\n")
+  cat("method for 'small samples.'\n")
   cat("\n")
   cat(x$X, "successes out of", x$n, "trials.\n")
   cat("\t Estimated proportion:", format(x$X / x$n, digits = digits), "\n")
