2637b8eee59ca8a550712198b8c712ca83316c24
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-24 19:37:56 +0000

    Delete test
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@106 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test/Ch21.R
+++
@@ -1,7 +0,0 @@
-# Chapter 21
-
-setwd("~/Dropbox/Classes/BIOL 490 - 2010-01 Biometry/Whitlock/abd/pkg")
-
-##########################################################################
-# selection
-load("data/selection.rda")
