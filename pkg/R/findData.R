187afd9f104addee71af860b9eae415d54a8a3c5
rpruim <rpruim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-06 06:28:48 +0000

    added findData()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/findData.R
@@ -0,0 +1,36 @@
+
+findData <- function(..., 
+	chapters=1:21, 
+	types= c('Example','Problem'),
+	numbers=1:100, 
+	pattern = '*',
+	ignore.case=TRUE) {
+
+	dots <- list(...)
+
+	for (x in dots) {
+		if ( all( x %in% c('Example','Problem') ) ) { 
+			types <- x 
+		} else { 
+			if (is.character(x)) { 
+				pattern <- x 
+			} else { 
+				if (is.numeric(x)) { 
+					chapters <- x 
+				} 
+			}
+		}
+	}
+
+
+	results <- subset(dataInfo,
+				chapter %in% chapters & 
+				type %in% types &
+				number %in% numbers &
+				grepl(pattern,name,ignore.case=ignore.case) 
+	)
+
+	if (prod(dim(results)) == 0) { return (NULL) }
+	return(results)
+}
+
