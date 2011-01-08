686ff1feea0239df8d56849f691ef9c029719e75
rpruim <rpruim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-08 21:30:38 +0000

    removed quantiles and added median to get shorter output
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abd@131 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/favstats.R
+++ pkg/R/favstats.R
@@ -1,13 +1,13 @@
 favstats <-
 function (x, na.rm = TRUE) 
 {
-    qq <- quantile(x, na.rm = na.rm)
-    val <- c(qq, 
+    val <- c(
+		median(x, na.rm=na.rm),
 		IQR(x, na.rm=na.rm),
 		mean(x, na.rm = na.rm), 
 		sd(x, na.rm = na.rm), 
         var(x, na.rm = na.rm)
 		)
-    names(val) <- c(names(qq), "IQR", "mean", "sd", "var")
+    names(val) <- c("median", "IQR", "mean", "sd", "var")
     return(val)
 }
