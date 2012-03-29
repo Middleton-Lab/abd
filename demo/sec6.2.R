76877ecd247c3d3eb64744534caabdcdc7d06dae
Randall Pruim <rpruim@gmail.com>
2012-03-29 16:57:22 -0400

    prep for CRAN 0.2 release

--- demo/sec6.2.R
+++ demo/sec6.2.R
@@ -6,6 +6,10 @@ cbind( right.handed=0:18, prob=round(dbinom(0:18, 18, 0.5),6))
 
 xyplot(dbinom(0:18,18,0.5) ~ 0:18, groups=0:18 %in% 5:13, 
 	type='h', lwd=8, lty=1)
-histochart(dbinom(0:18,18,0.5) ~ 0:18, groups=0:18 %in% 5:13) 
-propPval(14,18,.5)
+
+plotDist("binom", params=c(18,.5), groups= x %in% 5:13, 
+	type='h', lwd=8, lty=1, par.settings=col.mosaic())
+plotDist('binom', params=c(18,.5), kind='histogram', groups = x %in% 5:13, 
+		 par.settings=col.mosaic())
+pval(binom.test(14,18,.5))
 
--- demo/sec6.5.R
+++ demo/sec6.5.R
@@ -3,5 +3,5 @@
 xyplot(dbinom(0:18,18,0.5) ~ 0:18, type='h', 
 	groups=0:18 %in% 0:12, lwd=8, lty=1)
 
-histochart(dbinom(0:18,18,0.5) ~ 0:18, groups=0:18 %in% 0:12) 
+plotDist('binom', params=c(18,0.5), groups=x %in% 0:12, kind='histogram') 
 
