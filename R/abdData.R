fccc4293dfbaeeef7817cde4e446c6f85d4db480
Kevin Middleton <middletonk@missouri.edu>
2015-02-01 14:00:49 -0600

    Fix "No visible binding..." NOTE

--- R/abdData.R
+++ R/abdData.R
@@ -35,34 +35,38 @@
 #' abdData('Exercise')
 #' 
 abdData <- function(..., 
-	chapters = 1:21, 
-	types = c('Example', 'Problem'),
-	numbers = 1:100, 
-	pattern = '*',
-	ignore.case = TRUE) {
-
-	dots <- list(...)
-
-	for (x in dots) {
-		if ( all( x %in% c('Example','Problem') ) ) { 
-			types <- x 
-		} else { 
-			if (is.character(x)) { 
-				pattern <- x 
-			} else { 
-				if (is.numeric(x)) { 
-					chapters <- x 
-				} 
-			}
-		}
-	}
-
-	results <- dataInfo[dataInfo$chapter %in% chapters & 
-	  dataInfo$type %in% types &
-	  dataInfo$number %in% numbers &
-    grepl(pattern,dataInfo$name,ignore.case=ignore.case), ]
-
-	if (prod(dim(results)) == 0) { return (NULL) }
-	return(results)
+                    chapters = 1:21, 
+                    types = c('Example', 'Problem'),
+                    numbers = 1:100, 
+                    pattern = '*',
+                    ignore.case = TRUE) {
+  
+  dots <- list(...)
+  
+  for (x in dots) {
+    if ( all( x %in% c('Example','Problem') ) ) { 
+      types <- x 
+    } else { 
+      if (is.character(x)) { 
+        pattern <- x 
+      } else { 
+        if (is.numeric(x)) { 
+          chapters <- x 
+        } 
+      }
+    }
+  }
+  
+  results <- with(
+    abd::dataInfo,
+    dataInfo[chapter %in% chapters & 
+               type %in% types &
+               number %in% numbers &
+               grepl(pattern,
+                     name,
+                     ignore.case = ignore.case), ])
+  
+  if (prod(dim(results)) == 0) { return (NULL) }
+  return(results)
 }
 
